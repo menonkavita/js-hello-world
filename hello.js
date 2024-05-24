662b762b11bcb7248983b8a14efe77461f0bcd16
K M <menon_kavita@yahoo.com>
2024-05-23 22:56:28 -0500

    Chapter 2 - Hello World Exercise

--- hello.js
+++ hello.js
@@ -5,6 +5,19 @@ to the console (the panel on the right)
 */
 
 console.log("Hello");
+console.log('Hello World 2');
+console.log("Hello World without semi-colon")
+console.log("Number of the day is 10");
+console.log("Sum of numbers 1 and 7 is not : " + 1 + 7);
+console.log("Sum of numbers 1 and 7 is : " + (1 + 7));
+console.log("Printing Message 1 - Hello! " + " Printing Message 2 - Yellow!");
+console.log('"Women belong in all places where decisions are being made. It should not be that women are the exception." - Ruth Bader Ginsburg');
+console.log("Above message without use of Apostrophe.    Below message with Apostrophe.");
+console.log('"Women belong in all places where decisions are being made. It \shouldn\'t be that women are the exception." - Ruth Bader Ginsburg');
+
+/*console.log(Hello World without any quotes); Did not work, gave syntax error.
+
+
 
 /*  
 Next, PLAY! 
@@ -19,4 +32,5 @@ Modify your code on line 7 to try to accomplish the tasks listed below:
    7. Print two messages on the same line.
    8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
    9. Other. You choose!
+   10. K Suggestion - Try to print Poetry with line breaks, Image etc.
 */
